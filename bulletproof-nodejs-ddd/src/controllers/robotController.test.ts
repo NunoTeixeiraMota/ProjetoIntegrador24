8d101c0695d725210be31ac61f816399bf7db6a1
1210819 <1210819@isep.ipp.pt>
2023-10-28 22:56:27 +0100

    Correction to integration test

--- bulletproof-nodejs-ddd/src/controllers/robotController.test.ts
+++ bulletproof-nodejs-ddd/src/controllers/robotController.test.ts
@@ -3,7 +3,6 @@ import * as sinon from 'sinon';
 import { Response, Request, NextFunction } from 'express';
 
 import { Container } from 'typedi';
-import config from "../../config";
 
 import { Result } from '../core/logic/Result';
 import robotController from './robotController';
@@ -13,14 +12,14 @@ import IRobotTypeDTO from '../dto/IRobotTypeDTO';
 describe('RobotController (Integration Test)', function () {
   beforeEach(function() {
     Container.reset();
-    const robotTypeSchemaInstance = require("../src/persistence/schemas/robotTypeSchema").default;
+    const robotTypeSchemaInstance = require("../persistence/schemas/robotTypeSchema").default;
     Container.set("robotTypeSchema", robotTypeSchemaInstance);
 
-    const robotTypeRepoClass = require("../src/repos/robotTypeRepo").default;
+    const robotTypeRepoClass = require("../repos/robotTypeRepo").default;
     const robotTypeRepoInstance = new robotTypeRepoClass();
     Container.set("robotTypeRepo", robotTypeRepoInstance);
 
-    const robotServiceClass = require("../src/services/robotService").default;
+    const robotServiceClass = require("../services/robotService").default;
     const robotServiceInstance = new robotServiceClass();
     Container.set("robotService", robotServiceInstance);
   });
