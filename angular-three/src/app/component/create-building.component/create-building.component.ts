446fc229293ce773d641ad3fa17ab61bf9ba2d3a
1200941 <1200941@isep.ipp.pt>
2023-11-19 00:18:43 +0000

    building component angular

--- angular-three/src/app/component/create-building.component/create-building.component.ts
+++ angular-three/src/app/component/create-building.component/create-building.component.ts
@@ -7,21 +7,28 @@ import { BuildingService } from '../../service/Building/building.service'
   styleUrls: ['./create-building.component.css']
 })
 export class CreateBuildingComponent {
+  buildingData = {
+    name: '',
+    localizationoncampus: '',
+    floors: 0,
+    lifts: 0,
+    maxCel: [5000] // Start with one default value or empty array
+  };
 
   constructor(private buildingService: BuildingService) {}
 
   createBuilding() {
-    const buildingData = {
-      name: "Science Building",
-      localizationoncampus: "North Campus",
-      floors: 5,
-      lifts: 2,
-      maxCel: [5000, 5200, 5400, 5600, 5800]
-    };
-
-    this.buildingService.createBuilding(buildingData).subscribe(
+    this.buildingService.createBuilding(this.buildingData).subscribe(
       response => console.log('Building created:', response),
       error => console.error('Error:', error)
     );
+  }
+
+  addMaxCel() {
+    this.buildingData.maxCel.push(0); // Add a new cell with a default value
+  }
+
+  removeMaxCel(index: number) {
+    this.buildingData.maxCel.splice(index, 1); // Remove the cell at the given index
   }
 }
