a359933f1a43f1b56d8680091a08b499aea9700a
João Veríssimo <1201806@isep.ipp.pt>
2023-11-20 18:27:09 +0000

    US 1060 - UI Implementation #87

--- angular-three/src/app/app-routing.module.ts
+++ angular-three/src/app/app-routing.module.ts
@@ -7,6 +7,7 @@ import { CreateBuildingComponent } from './component/create-building.component/c
 import { CreateFloorComponent } from './component/create-floor.component/create-floor.component';
 import { UpdateBuildingComponent } from './component/update-building.component/update-building.component';
 import { CreateLiftComponent } from './component/create-lift.component/create-lift.component';
+import {ListBuildingsComponent} from './component/list-buildings.component/list-buildings.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/main-menu', pathMatch: 'full' },
@@ -16,6 +17,7 @@ const routes: Routes = [
   { path: 'main-menu', component: MainMenuComponent },
   { path: 'update-building', component: UpdateBuildingComponent },
   { path: 'create-lift', component: CreateLiftComponent },
+  { path: 'list-buildings', component: ListBuildingsComponent },
 ];
 
 @NgModule({
