cb2c36f56644e8c52a44c822dbcafded11e2bb09
JoseTeixeira1200941 <1200941@isep.ipp.pt>
2023-12-25 03:46:27 +0000

    String connection since both servers are running on same server

--- angular-three/config.ts
+++ angular-three/config.ts
@@ -1,4 +1,4 @@
 export const API_CONFIG = {
   apiBaseUrl: 'http://vs447.dei.isep.ipp.pt:4000/api',
-  apiBaseUrlAuth: 'https://vs447.dei.isep.ipp.pt:7145'
+  apiBaseUrlAuth: 'http://vs447.dei.isep.ipp.pt:5054'
 };  
