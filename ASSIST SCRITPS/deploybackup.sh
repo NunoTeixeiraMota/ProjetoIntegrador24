dca08eea77d4e5438b64c1d4bd98ae98264cec9f
JoseTeixeira1200941 <1200941@isep.ipp.pt>
2023-12-29 02:19:40 +0000

    test dewploy sh

--- ASSIST SCRITPS/deploybackup.sh
+++ ASSIST SCRITPS/deploybackup.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-# Incluir o arquivo de configuração
-source ./config.sh
+# MongoDB connection string
+MONGO_CONN_STRING="mongodb://mongoadmin:105711abb1e672194c53cbe4@vsgate-s1.dei.isep.ipp.pt:11147/?authMechanism=DEFAULT"
+
+# Set the backup path
+BACKUP_PATH="/srv/MongoDB/Backup" # Update this to your backups directory
 
 # Lista os backups disponíveis
 echo "Backups disponíveis:"
@@ -23,14 +26,14 @@ SELECTED_BACKUP="${BACKUPS[SELECTED_BACKUP_NUM]}"
 
 # Restaura o backup selecionado
 echo "Restaurando o backup selecionado: $SELECTED_BACKUP"
-mongorestore --host $MONGO_HOST --port $MONGO_PORT --username $MONGO_USER --password $MONGO_PASSWORD --db $MONGO_DB $BACKUP_PATH/$SELECTED_BACKUP/$MONGO_DB
+mongorestore --uri "$MONGO_CONN_STRING" $BACKUP_PATH/$SELECTED_BACKUP
 
 # Verifica se a restauração foi bem-sucedida
 if [ $? -eq 0 ]; then
-  echo "Restauração do banco de dados $DB_NAME concluída com sucesso."
+  echo "Restauração do banco de dados concluída com sucesso."
 
   # Verifique a existência da coleção "buildings" no banco de dados restaurado
-  EXISTS_BUILDINGS=$(mongo --host $MONGO_HOST --port $MONGO_PORT --username $MONGO_USER --password $MONGO_PASSWORD --eval "db.getCollectionNames().indexOf('buildings') != -1")
+  EXISTS_BUILDINGS=$(mongo "$MONGO_CONN_STRING" --eval "db.getCollectionNames().indexOf('buildings') != -1")
 
   if [ "$EXISTS_BUILDINGS" = "true" ]; then
     echo "A coleção 'buildings' foi restaurada com sucesso no banco de dados."
@@ -38,19 +41,9 @@ if [ $? -eq 0 ]; then
     echo "Erro: A coleção 'buildings' não foi encontrada após a restauração do banco de dados."
   fi
 
-  # Verifique o número de coleções no backup
-  COLLECTION_COUNT_BACKUP=$(ls -1 $BACKUP_PATH/$SELECTED_BACKUP/$MONGO_DB | grep '.bson' | wc -l)
-
-  # Verifique o número atual de coleções no banco de dados
-  COLLECTION_COUNT_CURRENT=$(mongo --host $MONGO_HOST --port $MONGO_PORT --username $MONGO_USER --password $MONGO_PASSWORD --eval "db.getCollectionNames().length")
-
-  if [ "$COLLECTION_COUNT_BACKUP" -eq "$COLLECTION_COUNT_CURRENT" ]; then
-    echo "O número de coleções no backup é igual ao número atual no banco de dados."
-  else
-    echo "Erro: O número de coleções no backup não corresponde ao número atual no banco de dados."
-  fi
+  # Outras verificações podem ser adicionadas aqui conforme necessário
 
 else
-  echo "Erro durante a restauração do banco de dados $DB_NAME."
+  echo "Erro durante a restauração do banco de dados."
   exit 1
 fi
