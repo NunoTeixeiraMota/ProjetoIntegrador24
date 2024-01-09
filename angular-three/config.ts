03f0300aa948698d2d3ac3d67472ddef78a5d0fc
1200941 <1200941@isep.ipp.pt>
2024-01-09 14:37:19 +0000

    Chart

--- angular-three/config.ts
+++ angular-three/config.ts
@@ -1,4 +1,4 @@
 export const API_CONFIG = {
-  apiBaseUrl: 'http://vs447.dei.isep.ipp.pt:4000/api',
+  apiBaseUrl: 'http://localhost:4000/api',
   apiBaseUrlAuth: 'https://robdroneandgo.azurewebsites.net'
-}
+} 
